34bc6c017efb24dd6cc143fa7626abf23b50ccc5
Serhiy K <mrlanu@gmail.com>
2018-03-26 00:17:48 -0500

    DownloadLogger class, write info about downloads

--- src/client_server/server/Server.java
+++ src/client_server/server/Server.java
@@ -5,21 +5,21 @@ import client_server.ConsoleHelper;
 import client_server.Message;
 import client_server.MessageType;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class Server {
 
     private static Path directory;
     private static int port;
+    private static Map<String, Integer> fileDownloadLogger;
 
     public static void main(String[] args) {
         init();
@@ -32,12 +32,20 @@ public class Server {
         ConsoleHelper.writeMessage("--->>> Please enter a directory : ");
         directory = Paths.get(ConsoleHelper.readString());
 
+        // put all files in map from directory for count download events
+        fileDownloadLogger = new HashMap<>();
+        getFilesList(directory).stream()
+                .forEach(p -> fileDownloadLogger.put(p.getFileName().toString(), 0));
+
         try (ServerSocket serverSocket = new ServerSocket(port))
         {
             ConsoleHelper.writeMessage
                     (String.format("\n--->>> The Server has started on the port : %s, directory - %s", port, directory));
             while (true){
                 new Handler(serverSocket.accept()).start();
+                DownloadLogger dl = new DownloadLogger();
+               // dl.setDaemon(true);
+                dl.start();
             }
 
         } catch (IOException e) {
@@ -107,11 +115,48 @@ public class Server {
                     try(FileInputStream fis = new FileInputStream(fileForSending.toFile())) {
                         byte[] buffer = new byte[(int) fileForSending.toFile().length()];
                         fis.read(buffer);
-                        connection.send(new Message(MessageType.FILE_BY_NAME, fileForSending.getFileName().toString(), buffer));
+                        connection.send(new Message(MessageType.FILE_BY_NAME, fileName, buffer));
+                        fileDownloadLogger.replace(fileName, fileDownloadLogger.get(fileName) + 1);
+                        fileDownloadLogger
+                                .forEach((k, v) -> System.out.println(String.format("File name : %s, downloaded : %d times", k, v)));
                     }
 
                 }else {
                     ConsoleHelper.writeMessage("--->>> Can't recognize this command.");
+                }
+            }
+        }
+    }
+
+    private static class DownloadLogger extends Thread{
+
+        private final Path LOGGER_PATH;
+
+        public DownloadLogger(){
+            this.LOGGER_PATH = Paths.get("c:/txt/Lanu/logger.txt");
+        }
+
+        @Override
+        public void run() {
+
+            while (true) {
+                try (BufferedWriter writer = Files.newBufferedWriter(LOGGER_PATH, StandardOpenOption.APPEND)){
+                    TimeUnit.SECONDS.sleep(30);
+
+                    // for delete file content
+                    PrintWriter pw = new PrintWriter(LOGGER_PATH.toFile());
+                    pw.close();
+
+                    fileDownloadLogger.forEach((k, v) -> {
+                        try {
+                            writer.write(String.format("File : %s downloaded : %d times\n", k, v));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+
+                } catch (InterruptedException | IOException e) {
+                    e.printStackTrace();
                 }
             }
         }
